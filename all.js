368bdbc1054acf9d6d4d4dbc98d8f5900da02f2f
rock43053 <agm.baf@gmail.com>
2020-07-02 20:01:42 -0400

    Group classwork

--- all.js
+++ all.js
@@ -1,6 +1,18 @@
 function all(arr) {
 	// Loop through every item in the array
+	for(let i =0; i < arr.length; i++) {
 	// If the item evaluates to false, return false
+	if (arr[i] == false) {
+		return false
+	}
 
+}
+return true
+	
+		
+	
 	// If the loop ends, return true
 }
+
+let array1 = [true, false, true]
+console.log(all(array1))
